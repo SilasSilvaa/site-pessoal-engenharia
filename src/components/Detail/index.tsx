33fdb2db707e67fef7646b55e49ca2984d820d04
Silas Oliveira Silva <silsilva@uolinc.com>
2023-02-15 17:20:30 -0300

    .

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,12 +1,31 @@
 
+import Image from 'next/image'
+import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 
-export default function Detail({conteudo, close}){
+type DataProject = {
+    concat: any
+    map(arg0: (projeto: any) => JSX.Element): import("react").ReactNode
+    uid: string,
+    image: string,
+    title: string,
+    description: string,
+    date: string
+}
+
+interface Project{
+ conteudo: DataProject,
+ close: any
+}
+
+export default function Detail({conteudo, close}: Project){
     return(
         <div>
-            <button onClick={close}>X</button>
+            <FiX onClick={close} size={25} color="#FFF"/>
             <h1>{conteudo.title}</h1>
+            <Image src={conteudo.image} alt={conteudo.title} width={500} height={500}/>
+            <span>{conteudo.description}</span>
         </div>
     )
 }
